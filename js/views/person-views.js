682244d2c21e43f478130281a56ba0685db2ee4f
adaohauzoho <adaohauzoho@Adaohas-MacBook-Air.local>
2016-04-27 18:03:00 -0400

    templates linked at the bottom of file

--- js/views/person-views.js
+++ js/views/person-views.js
@@ -10,4 +10,35 @@ app.views.Person = Backbone.View.extend({
 
 
 	template: _.template($('#person-template').html()),
+
+	render: function() {
+		this.$el.html(this.template(this.model.toJSON()));
+		return this;
+	},
+
+	app.views.People = Backbone.View.extend({
+
+	el: '#wrapper',
+	
+	initialize: function(data) {
+		this.collection = new app.collections.People(data);
+		this.render();
+
+	},
+
+	render: function() {
+		var self = this;
+		$('#listing').empty();
+		_.each(this.collection.models, function(person) {
+			self.renderPerson(person);
+		}, this);
+	},
+
+	renderPerson: function(person) {
+		var newperson = new app.views.Person({
+			model: person
+		});
+		$('#listing').append(newperson.render().el);
+
+	},
 });

c0216c48eb46a98b24925ee7d1c5f3a5b27141f4
adaohauzoho <adaohauzoho@Adaohas-MacBook-Air.local>
2016-04-27 16:58:40 -0400

    initial commit of person-model.js

---
+++ js/models/person-model.js
@@ -0,0 +1,49 @@
+app = app || {};
+
+app.models.Person = Backbone.Model.extend({
+	defaults: {
+		'firstname': '',
+		'lastname': '',
+		'number': '',
+		'cuisine': '',
+		'maxspent': ''
+	},
+	
+	initialize: function() {
+		var self = this;
+		if(this.get('cuisine') == 'mexican') {
+			self.set('type', 'mexican');
+
+		if(this.get('cuisine') == 'nigerian') {
+			self.set('type', 'nigerian');
+
+		if(this.get('cuisine') == 'thai') {
+			self.set('type', 'thai');
+
+		if(this.get('cuisine') == 'chinese') {
+			self.set('type', 'chinese');
+
+
+		if(this.get('cuisine') == 'vietnamese') {
+			self.set('type', 'vietnamese');
+
+		if(this.get('cuisine') == 'jamaican') {
+			self.set('type', 'jamaican');
+
+		} else {
+			self.set('type', 'No Preference');
+		}
+	}
+	
+});
+
+app.collections.People = Backbone.Collection.extend({
+	
+	model: app.models.Person,
+	
+	comparator: function(person) {
+		return person.get('lastname');
+
+	}
+	
+});
